94b1d4200832ea68a78921c853478458dd480949
Kim Ji Seong <jsk07aa@gmail.com>
2024-05-23 01:59:38 +0900

    Make Tile Generation
    40*40 크기의 타일 생성, 랜덤으로 타일 높이 변화 테스트

--- RogueLike_Project/Assets/Scripts/TileManager.cs
+++ RogueLike_Project/Assets/Scripts/TileManager.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    Tile[,] tiles = new Tile[20,20];
+    Tile[,] tiles = new Tile[40,40];
     [SerializeField] GameObject tile;
     // Start is called before the first frame update
     void Start()
     {
-        //tile.MovePosition(2.5f);
-        //tile.ChangeHeight(5f, 4);
+        GenerateTiles();
+        StartCoroutine(RandomChangeHeight(20));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GenerateTiles()
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            for (int j = 0; j < 40; j++)
+            {
+                tiles[i, j] = Instantiate(tile, new Vector3(i * 2, 0, j * 2), Quaternion.identity, this.transform).GetComponent<Tile>();
+            }
+        }
+    }
+
+    IEnumerator RandomChangeHeight(int iteration)
+    {
+        for(int i = 0; i<iteration; i++)
+        {
+            //int randNum = Random.Range(10, 20);
+            for(int j = 0; j < 30; j++)
+            {
+                int x = Random.Range(0, 40);
+                int y = Random.Range(0, 40);
+                tiles[x, y].ChangeHeight(Random.Range(2, 20));
+            }
+            yield return new WaitForSeconds(4f);
+        }
     }
 }
