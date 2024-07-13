1b23a04a3de2e5b93f586dc20713869c34a13f40
IsaacKimForGitHub <isaac0112@naver.com>
2024-07-13 14:13:38 +0900

    PauseMenu
    일시중지 메뉴 만듬
    그 외 화면 구성 변경

--- RogueLike_Project/Assets/Scripts/UIScript/DispChange.cs
+++ RogueLike_Project/Assets/Scripts/UIScript/DispChange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SettingManager : MonoBehaviour
+public class DispChange : MonoBehaviour
 {
     public GameObject CurDisp, NextDisp;
 
