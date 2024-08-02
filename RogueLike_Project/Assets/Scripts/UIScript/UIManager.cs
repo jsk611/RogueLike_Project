0e488788aa58705ad54fde0c879c6951fe2da0ee
IsaacKimForGitHub <isaac0112@naver.com>
2024-08-03 07:57:12 +0900

    Opti_2
    TitleButtonManager 삭제 및
    UIManager에 병합
    ESC 눌렀을 때 PauseMenu 뜨도록 설정(게임이 물리적으로 정지하는 건 아직 구현 X)

--- RogueLike_Project/Assets/Scripts/UIScript/UIManager.cs
+++ RogueLike_Project/Assets/Scripts/UIScript/UIManager.cs
@@ -2,14 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            PauseGame();
+    }
+
     public Slider[] Bar;
     public float[] maxValue;
     public float[] currentValue;
     public void BarValueChange(int i)
     {
         Bar[i].value = currentValue[i] / maxValue[i];
+    }
+
+    public void InitGame()
+    {
+        SceneManager.LoadScene("Prototype1");
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public GameObject PauseCanvas;
+    public void PauseGame()
+    {
+        PauseCanvas.SetActive(true);
     }
 }
