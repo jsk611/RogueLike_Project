d22da8332e4ca86d46f07699e5c515e1617c6643
IsaacKimForGitHub <isaac0112@naver.com>
2024-08-07 22:27:26 +0900

    Opti_3
    UIManager 스크립트로 전체 병합 완료
    Cooltime 구현 시 설정 시간과 실제 시간 사이 차이 문제 개선
    Swapping 시 불필요한 UI 이동 삭제
    현 시점 최적화 작업은 완료

--- RogueLike_Project/Assets/Scripts/UIScript/UIManager.cs
+++ RogueLike_Project/Assets/Scripts/UIScript/UIManager.cs
@@ -6,12 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private void Start()
+    {
+        CoinReset(23456);
+        StartCoroutine(OnCooltime(5));
+        Swapping(0);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             PauseGame();
     }
 
+    //Controlling bars value
     public Slider[] Bar;
     public float[] maxValue;
     public float[] currentValue;
@@ -20,6 +28,7 @@ public class UIManager : MonoBehaviour
         Bar[i].value = currentValue[i] / maxValue[i];
     }
 
+    //TitleScene(Start, Quit)
     public void InitGame()
     {
         SceneManager.LoadScene("Prototype1");
@@ -33,9 +42,51 @@ public class UIManager : MonoBehaviour
         #endif
     }
 
+    //Pausing game
     public GameObject PauseCanvas;
     public void PauseGame()
     {
         PauseCanvas.SetActive(true);
+    }
+
+    //Swapping Weapons
+    public GameObject[] weapon;
+    public void Swapping(int index)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (index == i)
+            {
+                weapon[i].transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+            }
+            else
+            {
+                weapon[i].transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+            }
+        }
+    }
+
+    public Text dnaText;
+    int dna = 0;
+    public void CoinReset(int getDNA)
+    {
+        dna += getDNA;
+        dnaText.text = GetThousandCommaText(dna).ToString();
+    }
+    public string GetThousandCommaText(int data)
+    {
+        return string.Format("{0:#,###}", data);
+    }
+
+    public Image deactivateImage;
+    public IEnumerator OnCooltime(float cool)
+    {
+        float curcool = cool;
+        while (curcool > 0)
+        {
+            curcool -= Time.deltaTime;
+            deactivateImage.fillAmount = (curcool / cool);
+            yield return null;
+        }
     }
 }
