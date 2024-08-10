36e5835e195e30ecf978da41a674bae0a023e477
IsaacKimForGitHub <isaac0112@naver.com>
2024-08-10 14:57:08 +0900

    SkillTree Update 1
    스킬트리 클릭 시 노드 활성화 구현
    이후 추가 구현 예정

--- RogueLike_Project/Assets/Scripts/UIScript/SkilltreeManager.cs
+++ RogueLike_Project/Assets/Scripts/UIScript/SkilltreeManager.cs
@@ -5,5 +5,40 @@ using UnityEngine.UI;
 
 public class SkilltreeManager : MonoBehaviour
 {
+    public Button[] attackButton;
+    Button curattackButton;
+    int alevel = 2;
 
+    private void Start()
+    {
+        curattackButton = attackButton[alevel];
+        for(int i = 0; i < alevel; i++)
+        {
+            ButtonActivated(i);
+        }
+        curattackButton.interactable = true;
+        curattackButton.onClick.AddListener(OnClickStart);
+    }
+
+    void OnClickStart()
+    {
+        curattackButton.interactable = false;
+        ButtonActivated(alevel);
+        curattackButton.onClick.RemoveListener(OnClickStart);
+
+        alevel++;
+        if (alevel < attackButton.Length)
+        {
+            curattackButton = attackButton[alevel];
+            curattackButton.interactable = true;
+            curattackButton.onClick.AddListener(OnClickStart);
+        }
+    }
+
+    void ButtonActivated(int i)
+    {
+        ColorBlock colorBlock = attackButton[i].colors;
+        colorBlock.disabledColor = Color.yellow;
+        attackButton[i].colors = colorBlock;
+    }
 }
