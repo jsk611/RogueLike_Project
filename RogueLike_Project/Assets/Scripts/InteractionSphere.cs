b424eff8de8326199264ff01fd79acae72785523
DTCircle <dongri1108@gmail.com>
2024-08-20 17:42:30 +0900

    Wave Interaction Sphere (1)
    사용하지 않는 스크립트 정리

--- RogueLike_Project/Assets/Scripts/InteractionSphere.cs
+++ RogueLike_Project/Assets/Scripts/InteractionSphere.cs
@@ -12,9 +12,9 @@ public class InteractionSphere : MonoBehaviour
         sphereCollider = GetComponent<SphereCollider>();
     }
 
-    // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
+        //플레이어를 감지했을 때 버튼입력을 대기하는 코루틴 실행
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Can Switch Wave");
@@ -22,21 +22,25 @@ public class InteractionSphere : MonoBehaviour
         }
 
     }
+
+    
     private void OnTriggerExit(Collider other)
     {
+        //플레이어가 범위에서 벗어났을 때 입력감지 중단
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Cannot Switch Wave");
             StopCoroutine("PressToSwitchWave");
         }
     }
-    //
+    
     IEnumerator PressToSwitchWave()
     {
         while (true)
         {
             if(Input.GetKey(KeyCode.F))
             {
+                // F 입력했을 때 신호 송신
                 Debug.Log("Switching!");
                 Destroy(gameObject);
             }
