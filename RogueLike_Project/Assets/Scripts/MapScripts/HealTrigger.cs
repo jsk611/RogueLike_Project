98bc8a9281be692c41caade57d329ff6c9ab4600
Kim Ji Seong <jsk07aa@gmail.com>
2024-10-30 18:56:07 +0900

    Heal in MaintainStage
    정비 스테이지에서 체력 회복 가능하게 구현 (최대 체력의 30%)

---
+++ RogueLike_Project/Assets/Scripts/MapScripts/HealTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealTrigger : MonoBehaviour
+{
+    bool isHealed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHealed = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!isHealed && Input.GetKeyDown(KeyCode.F))
+            {
+                isHealed = true;
+                PlayerStatus ps = other.gameObject.GetComponent<PlayerStatus>();
+                ps.IncreaseHealth(ps.GetMaxHealth() * 0.3f);
+                GetComponent<MeshRenderer>().material.color *= new Color(1, 1, 1, 0.5f);
+            }
+        }
+    }
+    
+}
