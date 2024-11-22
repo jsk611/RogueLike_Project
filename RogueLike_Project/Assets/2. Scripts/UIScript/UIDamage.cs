b0ecd0a7d377d881684f21837af5504eba5dcb4c
JSKim <jsk07aa@gmail.com>
2024-11-22 20:12:01 +0900

    Make Damage UI
    데미지 표시 UI 제작

---
+++ RogueLike_Project/Assets/2. Scripts/UIScript/UIDamage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class UIDamage : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    [SerializeField] Transform player;
+    [SerializeField] float initialScale = 1f;
+    public float damage = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //text = GetComponentInChildren<TMP_Text>();
+        text.text = damage.ToString();
+        Destroy(gameObject, 1f);
+        player = FindObjectOfType<PlayerControl>().gameObject.transform;
+    }
+    private void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        // 크기 조정
+        float scale = distance * initialScale;
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+}
