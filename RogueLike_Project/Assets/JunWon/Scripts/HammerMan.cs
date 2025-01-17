fcd17631bb7f24341ecddc514f60a631583cfa4f
DTCircle <dongri1108@gmail.com>
2025-01-17 21:16:18 +0900

    Hammer? Man
    망치 들고 날뛰는 오함마맨 등장
    
    점프쿨마다 붕 뛰어서 사용자 앞으로 돌진함.
    착지했을 때 충격파가 발생해야 함.
    만들기 귀찮으므로 여기까지

--- RogueLike_Project/Assets/JunWon/Scripts/HammerMan.cs
+++ RogueLike_Project/Assets/JunWon/Scripts/HammerMan.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -14,9 +15,12 @@ public class HammerMan : MonsterBase
     private bool canJump = true;
     private Rigidbody rb;
 
+    private CapsuleCollider collider;
+
     protected override void Start()
     {
         base.Start();
+        collider = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
         nmAgent.updatePosition = false;
         nmAgent.updateRotation = false;
@@ -30,10 +34,10 @@ public class HammerMan : MonsterBase
             return;
         }
 
-        if (Vector3.Distance(transform.position, target.position) <= attackRange)
-        {
-            ChangeState(State.ATTACK);
-        }
+        //if (Vector3.Distance(transform.position, target.position) <= attackRange)
+        //{
+        //    ChangeState(State.ATTACK);
+        //}
 
         TryJump();
     }
@@ -44,7 +48,8 @@ public class HammerMan : MonsterBase
 
         isJumping = true;
         canJump = false;
-
+        anim.SetBool("CanJump", canJump);
+        StartCoroutine(MoveInAir());
         // 점프 로직
         Vector3 dir = (target.position - transform.position).normalized;
         Debug.Log("direction: " +  "{" + dir.x + " " + dir.y + " " + dir.z + "}");
@@ -58,6 +63,7 @@ public class HammerMan : MonsterBase
     {
         // 착지 확인
         if (((1 << collision.gameObject.layer) & groundLayer) != 0)
+            //if (Physics.SphereCast(transform.position, collider.radius - 0.06f;,-transform.up,0.2f,LayerMask.NameToLayer("Wall")))
         {
             OnLanding();
         }
@@ -77,10 +83,23 @@ public class HammerMan : MonsterBase
     private void ResetJumpCooldown()
     {
         canJump = true;
+        anim.SetBool("CanJump", canJump);
     }
 
     private void CreateShockwave(Vector3 position)
     {
         Instantiate(shockwavePrefab, position, Quaternion.identity);
+    }
+    IEnumerator MoveInAir()
+    {
+        while(isJumping)
+        {
+            Vector3 nav = (target.position-transform.position);
+            nav.y = 0;
+            
+            rb.AddForce(nav*chaseSpeed);
+            Debug.Log("mooooooving");
+            yield return null;
+        }
     }
 }
