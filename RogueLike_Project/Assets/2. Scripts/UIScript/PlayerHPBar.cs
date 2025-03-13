c64c5346cb0d13d692d8384b0b57f597fd1644c2
jsk <83943310+jsk611@users.noreply.github.com>
2025-03-14 02:01:56 +0900

    Upgrade PlayerStat UI
    플레이어 체력바, 스테미나, 재화 UI 디자인 개선

---
+++ RogueLike_Project/Assets/2. Scripts/UIScript/PlayerHPBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPBar : MonoBehaviour
+{
+    [SerializeField] Image main;
+    [SerializeField] Image mid;
+
+    float ratio = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ratio = main.fillAmount;
+
+        if (mid.fillAmount > ratio)
+        {
+            mid.fillAmount -= Time.deltaTime * 0.1f;
+        }
+        else if (mid.fillAmount < ratio)
+        {
+            mid.fillAmount = ratio;
+        }
+
+    }
+}
