3cd7179f6f1f2627bafcce687b998380389a56df
sonah5009 <168628117+IsaacKimForGitHub@users.noreply.github.com>
2025-05-28 15:54:09 +0900

    fix stage_1,2

--- RogueLike_Project/Assets/2. Scripts/MapScripts/EnemyType.cs
+++ RogueLike_Project/Assets/2. Scripts/MapScripts/EnemyType.cs
@@ -5,12 +5,12 @@ public enum EnemyType{  //등장시작스테이지-포지션(근접/기타:0, 원거리:1, 저격수:
     RangedSoldier=111,  //111
     Sniper=121,         //121
     Wormboss=131,       //131
-    HammerMan=201,      //201
     Turret=211,         //211
     Hoverbot,           //212
     Thrower,            //213
     Troyboss=231,       //231
-    Summoner=301,       //301
+    HammerMan=301,      //201
+    Summoner=302,       //301
     FieldMage=311,      //311
 
 }
