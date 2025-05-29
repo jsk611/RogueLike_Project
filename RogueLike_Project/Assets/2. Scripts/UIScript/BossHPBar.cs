85b62885216ff42163f3ee9163ec5ba2fae6ba05
JSKim <jsk07aa@gmail.com>
2025-05-29 19:03:28 +0900

    Make bossHPBars

--- RogueLike_Project/Assets/2. Scripts/UIScript/BossHPBar.cs
+++ RogueLike_Project/Assets/2. Scripts/UIScript/BossHPBar.cs
@@ -1,8 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BossHPBar : EnemyHPBar
+public class BossHPBar : MonoBehaviour
 {
+    [SerializeField] Image inside;
+    [SerializeField] Image mid;
+    [SerializeField] float speed = 0.2f;
+    [SerializeField] TMP_Text bossName;
+    BossStatus bossStatus;
 
+    float ratio = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetRatio(bossStatus.GetHealth(), bossStatus.GetMaxHealth());
+
+        inside.fillAmount = ratio;
+
+        if (mid.fillAmount > ratio)
+        {
+            mid.fillAmount -= Time.deltaTime * speed;
+        }
+        else mid.fillAmount = ratio;
+
+        //if (mid.fillAmount == 0) Destroy(gameObject);
+    }
+
+    void SetRatio(float current, float max)
+    {
+        ratio = current / max;
+    }
+
+    public void SetBoss(BossStatus boss)
+    {
+        bossStatus = boss;
+        bossName.text = boss.bossName;
+    }
 }
