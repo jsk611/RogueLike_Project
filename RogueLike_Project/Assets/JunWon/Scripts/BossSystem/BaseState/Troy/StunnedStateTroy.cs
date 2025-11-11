9f4afa64a2fc99da2cbe029cc584ead13b6fb858
DTCircle <dongri1108@gmail.com>
2025-11-11 19:02:07 +0900

    Horse Phase1 Complete
    미친 말대가리 1페이즈 완성

---
+++ RogueLike_Project/Assets/JunWon/Scripts/BossSystem/BaseState/Troy/StunnedStateTroy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunnedStateTroy : State<Troy>
+{
+    public StunnedStateTroy(Troy owner) : base(owner) { }
+
+    float stunTimer = 0;
+
+    public override void Enter()
+    {
+        stunTimer = 0;
+        owner.NmAgent.isStopped = true;
+        owner.lurkPhase = false;
+        owner.ChangeState(Troy.AnimatorState.Idle);
+    }
+    public override void Update()
+    {
+        stunTimer += Time.deltaTime;
+        if (stunTimer >= owner.STUNINVTERVAL) owner.ChangeState(Troy.AnimatorState.WakeUp);
+    }
+    public override void Exit()
+    {
+        owner.NmAgent.isStopped = false;
+    }  
+}
